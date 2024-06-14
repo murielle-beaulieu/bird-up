fed9b3c442ad1fd1be6d5481fae9eb1a6f35a8c2
Murielle Beaulieu <murielle.beaulieu19@gmail.com>
2024-06-14 12:38:47 +1000

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_044359) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
