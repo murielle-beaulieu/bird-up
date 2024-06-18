ce80e50ad203a49a39dd588ae5f3ab3a97e79003
Murielle Beaulieu <murielle.beaulieu19@gmail.com>
2024-06-18 14:36:55 +1000

    chat completed, added to dropdown menu

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,27 @@
+# class MessagesController < ApplicationController
+#   def create
+#     @chatroom = Chatroom.find(params[:chatroom_id])
+#     @message = Message.new(message_params)
+#     @message.chatroom = @chatroom
+#     @message.user = current_user
+#     if @message.save
+#       ChatroomChannel.broadcast_to(
+#           @chatroom,
+#           render_to_string(partial: "message", locals: { message: @message }, sender_id: @message.user.id)
+#         )
+#       head :ok
+#     else
+#       render "chatrooms/show", status: :unprocessable_entity
+#     end
+#   end
+
+#   private
+
+#   def message_params
+#     params.require(:message).permit(:content)
+#   end
+# end
+
 class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
@@ -7,7 +31,8 @@ class MessagesController < ApplicationController
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: {message: @message}),
+        sender_id: @message.user.id
       )
       head :ok
     else
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,28 +3,49 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["messages"]
 
-  #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  resetForm(event) {
+    event.target.reset()
   }
 
   connect() {
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => this.#insertMessageAndScrollDown(data) }
-    )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+      )
+      console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+      }
+
+    disconnect() {
+      console.log("Unsubscribed from the chatroom")
+      this.subscription.unsubscribe()
+    }
+
+  #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    }
+
+    #buildMessageElement(currentUserIsSender, message) {
+      console.log("buildMessageElement function:", currentUserIsSender)
+      return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+        ${message}
+        </div>
+      </div>
+      `
+    }
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
   }
 
-  resetForm(event) {
-    event.target.reset()
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender? "sender-style" : "receiver-style"
   }
 
-  disconnect() {
-    console.log("Unsubscribed from the chatroom")
-    this.subscription.unsubscribe()
-  }
 }
