1cfd18061d7ceec3eae4f95058064313a7556d37
Chris Nearing <clnearing@gmail.com>
2024-06-18 13:12:13 +0700

    no spottings welcome message

--- app/controllers/spottings_controller.rb
+++ app/controllers/spottings_controller.rb
@@ -3,14 +3,17 @@ class SpottingsController < ApplicationController
 
   def index
     @spottings = Spotting.where(user_id: current_user)
-
-    @markers = @spottings.geocoded.map do |spotting|
-      {
-        lat: spotting.latitude,
-        lng: spotting.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {spotting: spotting}),
-        marker_html: render_to_string(partial: "marker")
-      }
+    if @spottings == []
+      @birds = Bird.all
+    else
+      @markers = @spottings.geocoded.map do |spotting|
+        {
+          lat: spotting.latitude,
+          lng: spotting.longitude,
+          info_window_html: render_to_string(partial: "info_window", locals: {spotting: spotting}),
+          marker_html: render_to_string(partial: "marker")
+        }
+      end
     end
   end
 
